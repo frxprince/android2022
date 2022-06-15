7bb8fc4cb7a4464823090e5707cc386d0461c707
Paween Khoenkaw <frxprince@gmail.com>
2022-06-15 16:04:07 +0700

    working on binaryprefix

---
+++ BinaryPrefix/app/src/main/java/com/example/binaryprefix/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.binaryprefix
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn=findViewById<Button>(R.id.button)
+
+
+
+        btn.setOnClickListener(){
+            val i=Intent(this,InputPrefix::class.java)
+            startActivity(i)
+        }
+    }
+}
