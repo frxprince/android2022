849495b6c1811702c9c6c82e2c4f7841085f7724
Paween Khoenkaw <frxprince@gmail.com>
2022-06-16 21:00:51 +0700

    ...

---
+++ Money/app/src/main/java/com/example/money/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.money
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Website.URL
+import android.util.Log
+import android.widget.ArrayAdapter
+import org.jetbrains.anko.doAsync
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val adapter=ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
+        doAsync {
+            var url = URL("https://open.er-api.com/v6/latest/THB")
+            var conn = url.openConnection() as HttpURLConnection
+            conn.requestMethod = "GET"
+            conn.connect()
+            var response = BufferedReader(InputStreamReader(conn.inputStream)).readLine()
+            Log.v("response", response.toString())
+            var server_response= JSONObject(response)
+            var rates=server_response.getJSONObject("rates")
+
+            Log.v("test",rates.getString("USD"))
+            for (i in rates.keys()){
+                Log.v("test","$i -> ${rates.getDouble(i)}")
+            }
+        }
+
+    }
+}
